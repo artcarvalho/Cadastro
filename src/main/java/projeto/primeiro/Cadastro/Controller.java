c3090c68f5907880fb85946f26797984e71d4c79
Artur <arturcarvalho05@gmail.com>
2025-05-01 17:34:01 -0300

    Criando controller

---
+++ src/main/java/projeto/primeiro/Cadastro/Controller.java
@@ -0,0 +1,23 @@
+package projeto.primeiro.Cadastro;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+
+public class Controller {
+
+    @GetMapping("/boasVindas")
+    public String boasVindas(){
+        return "Bem Vindo!";
+    }
+
+    @GetMapping("/del/user")
+    public String delUser(){
+        return "User, deletado";
+    }
+
+}
