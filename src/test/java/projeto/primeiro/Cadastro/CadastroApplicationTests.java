ee5b70f61f02d0f5d6156b61913d4e172c212c3f
Artur <arturcarvalho05@gmail.com>
2025-05-01 13:44:05 -0300

    first commit

---
+++ src/test/java/projeto/primeiro/Cadastro/CadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.primeiro.Cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
